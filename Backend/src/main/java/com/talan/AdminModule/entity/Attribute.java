515b8db0733859be3551817d10b3cdbfd0984674
WaelArfaaoui <161303710+WaelArfaaoui@users.noreply.github.com>
2024-04-14 17:24:31 +0100

    update rule component

--- Backend/src/main/java/com/talan/AdminModule/entity/Attribute.java
+++ Backend/src/main/java/com/talan/AdminModule/entity/Attribute.java
@@ -1,4 +1,25 @@
 package com.talan.AdminModule.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "attribute")
 public class Attribute {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "attribute", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<RuleAttribute> ruleAttributes;
+
 }
--- Backend/src/main/java/com/talan/AdminModule/entity/Rule.java
+++ Backend/src/main/java/com/talan/AdminModule/entity/Rule.java
@@ -1,4 +1,30 @@
 package com.talan.AdminModule.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "rule")
 public class Rule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "category", nullable = false)
+    private String category;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "rule", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<RuleAttribute> ruleAttributes;
+
 }

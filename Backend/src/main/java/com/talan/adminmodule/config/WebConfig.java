7d1926a7a1b760505c44707af428cf6c49aab9f5
WaelArfaaoui <161303710+WaelArfaaoui@users.noreply.github.com>
2024-05-02 11:59:41 +0100

    update WebConfig class

--- Backend/src/main/java/com/talan/adminmodule/config/WebConfig.java
+++ Backend/src/main/java/com/talan/adminmodule/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:4200/*")
                         .allowedMethods(HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
                                 HttpMethod.DELETE.name())

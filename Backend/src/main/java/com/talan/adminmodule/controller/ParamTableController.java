3f24debdfa17a312504b809745249918a7fa6674
WaelArfaaoui <161303710+WaelArfaaoui@users.noreply.github.com>
2024-05-24 09:08:08 +0100

    update test classes

--- Backend/src/main/java/com/talan/adminmodule/controller/ParamTableController.java
+++ Backend/src/main/java/com/talan/adminmodule/controller/ParamTableController.java
@@ -2,6 +2,7 @@ package com.talan.adminmodule.controller;
 
 import com.talan.adminmodule.dto.*;
 import com.talan.adminmodule.entity.ParamAudit;
+import com.talan.adminmodule.service.ParamTableService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestWrapper;
